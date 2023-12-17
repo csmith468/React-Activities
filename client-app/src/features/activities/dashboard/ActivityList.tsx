b30b63848fa58be3df763a71b9cfac2dcf2596db
csmith468 <chapin2816@gmail.com>
2023-12-17 16:03:01 -0600

    Section 7: MobX (before mapping through 75)

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,15 +1,12 @@
 import { Button, Item, Label, Segment } from "semantic-ui-react";
-import { Activity } from "../../../app/models/activity";
 import { SyntheticEvent, useState } from "react";
+import { useStore } from "../../../app/stores/store";
+import { observer } from "mobx-react-lite";
 
-interface Props {
-  activities: Activity[];
-  selectActivity: (id: string) => void;
-  deleteActivity: (id: string) => void;
-  submitting: boolean;
-}
+export default observer(function ActivityList() {
+  const {activityStore} = useStore();
+  const {deleteActivity, activities, loading} = activityStore;
 
-export default function ActivityList({ activities, selectActivity, deleteActivity, submitting }: Props) {
   const [target, setTarget] = useState('');
 
   function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement>, id: string) {
@@ -33,14 +30,14 @@ export default function ActivityList({ activities, selectActivity, deleteActivit
               </Item.Description>
               <Item.Extra>
                 <Button
-                  onClick={() => selectActivity(activity.id)}
+                  onClick={() => activityStore.selectActivity(activity.id)}
                   floated="right"
                   content="View"
                   color="blue"
                 />
                 <Button
                   name={activity.id}
-                  loading={submitting && target == activity.id}
+                  loading={loading && target == activity.id}
                   onClick={(e) => handleActivityDelete(e, activity.id)}
                   floated="right"
                   content="Delete"
@@ -55,3 +52,4 @@ export default function ActivityList({ activities, selectActivity, deleteActivit
     </Segment>
   );
 }
+)
